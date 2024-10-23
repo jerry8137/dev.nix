91f6ee1fe4b782d462e412bdafea3cdd9f979252
Jerry Hou <chunyu.hou@itri.org.tw>
2024-10-23 08:03:15 +0800

    Add mamba init to zsh

--- module/zsh.nix
+++ module/zsh.nix
@@ -63,6 +63,19 @@
       fi
       unset __conda_setup
       # <<< conda initialize <<<
+
+      # >>> mamba initialize >>>
+      # !! Contents within this block are managed by 'micromamba shell init' !!
+      export MAMBA_EXE='/home/jerry/bin/micromamba';
+      export MAMBA_ROOT_PREFIX='/home/jerry/micromamba';
+      __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+      if [ $? -eq 0 ]; then
+          eval "$__mamba_setup"
+      else
+          alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+      fi
+      unset __mamba_setup
+      # <<< mamba initialize <<<
     '';
 
   };
