330609819d1fee4f9d784283431355b56fe76df3
Jerry Hou <chunyu.hou@itri.org.tw>
2025-02-19 15:29:59 +0800

    feat(fish): useful shell setup

--- module/fish.nix
+++ module/fish.nix
@@ -6,11 +6,13 @@
     shellAliases = {
       update = "nix run nixpkgs#home-manager -- switch --flake ~/kickstart.nix#x86_64-linux  --impure";
       ls = "eza --icons=auto";
+      config = "/run/current-system/sw/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME";
+      glol = "git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset\"";
+      glola = "git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset\" --all";
+    };
+    shellAbbrs = {
       ff = "fastfetch";
-      config="/run/current-system/sw/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME";
-      gst="git status";
-      glol="git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset\"";
-      glola="git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset\" --all";
+      gst = "git status";
     };
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
@@ -37,10 +39,130 @@
           /data/pcd-maps:/data/pcd-maps:ro \
       "
       set -x SDC_DOCKER_SSL_VERIFICATION_ENABLED false
+      set -x SDC_SHELL_ENV /home/jerry/.sdc-shell-env
       bind \cf 'tms'
       bind \cb 'tmux a'
       . "/home/jerry/miniconda3/etc/fish/conf.d/conda.fish"
+      if status is-login; or set -q SSH_TTY
+        fish_add_path --path /home/jerry/.nix-profile/bin
+        fish_add_path --path /home/jerry/.local/bin
+        fish_add_path --path /nix/var/nix/profiles/default/bin
+      end
     '';
+    functions = {
+      extract = '' 
+      function extract
+        switch $argv[1]
+            case "*.tar.bz2"
+                tar xjf $argv[1]
+
+            case "*.tar.gz"
+                tar xzf $argv[1]
+
+            case "*.bz2"
+                bunzip2 $argv[1]
+
+            case "*.rar"
+                unrar e $argv[1]
+
+            case "*.gz"
+                gunzip $argv[1]
+
+            case "*.tar"
+                tar xf $argv[1]
+
+            case "*.tbz2"
+                tar xjf $argv[1]
+
+            case "*.tgz"
+                tar xzf $argv[1]
+
+            case "*.zip"
+                unzip $argv[1]
+
+            case "*.Z"
+                uncompress $argv[1]
+
+            case "*.7z"
+                7z x $argv[1]
+
+            case "*"
+                echo "unknown extension: $argv[1]"
+        end
+      end
+      '';
+
+      extracttodir = '' 
+        function extracttodir
+          switch $argv[1]
+              case "*.tar.bz2"
+                  tar -xjf $argv[1] -C "$argv[2]"
+
+              case "*.tar.gz"
+                  tar -xzf $argv[1] -C "$argv[2]"
+
+              case "*.rar"
+                  unrar x $argv[1] "$argv[2]/"
+
+
+                  tar -xf $argv[1] -C "$argv[2]"
+
+              case "*.tbz2"
+                  tar -xjf $argv[1] -C "$argv[2]"
+
+              case "*.tgz"
+                  tar -xzf $argv[1] -C "$argv[2]"
+
+              case "*.zip"
+                  unzip $argv[1] -d $argv[2]
+
+              case "*.7z"
+                  7za e -y $argv[1] -o"$argv[2]"
+
+              case "*"
+                  echo "unknown extension: $argv[1]"
+          end
+        end
+      '';
+
+      ytarchive = '' 
+        function ytarchive
+         yt-dlp -f bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best -o '%(upload_date)s - %(channel)s - %(id)s - %(title)s.%(ext)s' \
+         --sponsorblock-mark "all" \
+         --geo-bypass \
+         --sub-langs 'all' \
+         --embed-subs \
+         --embed-metadata \
+         --convert-subs 'srt' \
+         --download-archive $argv[1].txt https://www.youtube.com/$argv[1]/videos; 
+        end
+      '';
+
+      ytarchivevideo = '' 
+        function ytarchivevideo
+          yt-dlp -f bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best -o '%(upload_date)s - %(channel)s - %(id)s - %(title)s.%(ext)s' \
+         --sponsorblock-mark "all" \
+         --geo-bypass \
+         --sub-langs 'all' \
+         --embed-metadata \
+         --convert-subs 'srt' \
+         --download-archive $argv[1] $argv[2]; 
+        end
+      '';
+
+      ytd = '' 
+        function ytd
+         yt-dlp -f bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best -o '%(upload_date)s - %(channel)s - %(id)s - %(title)s.%(ext)s' \
+         --sponsorblock-mark "all" \
+         --geo-bypass \
+         --sub-langs 'all' \
+         --embed-subs \
+         --embed-metadata \
+         --convert-subs 'srt' \
+         $argv
+        end
+      '';
+    };
     plugins = [
       # Enable a plugin (here grc for colorized command output) from nixpkgs
       { name = "grc"; src = pkgs.fishPlugins.grc.src; }
@@ -55,5 +177,9 @@
         };
       }
     ];
+  };
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
   };
 }
